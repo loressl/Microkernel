e1d26fc2710a2bb36339415598c83f5e1b118da2
loressl <loryssl@hotmail.com>
2019-07-13 15:30:21 -0300

    Adicionando arquivo xml, com o nome da classe que será usada para comparar, dentro de uma pasta no projeto.

--- Application/src/application/AutenticationController.java
+++ Application/src/application/AutenticationController.java
@@ -1,34 +1,52 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import interfaces.AutenticationBackEndCreator;
 import interfaces.IAutenticationController;
 import interfaces.ICore;
 
-
-public class AutenticationController implements IAutenticationController{
+public class AutenticationController implements IAutenticationController {
 
 	@Override
 	public boolean initialize(ICore core) {
-		String url= "C:\\Users\\maqui\\OneDrive\\Documentos\\ANÁLISE E DESENVOLVIMENTO DE SISTEMAS\\4º SEMESTRE\\PADROES\\trabalho 1\\AutenticationBackEndClass.txt";
+
 		try {
-			BufferedReader readFile= new BufferedReader(new FileReader(url));
-			String lineOfFile= readFile.readLine();
-			String classe = null;
-			while(lineOfFile != null) {
-				classe = lineOfFile.substring(lineOfFile.lastIndexOf("\\") + 1).split("\\.")[0];
-				lineOfFile= readFile.readLine();
+			File currentDir = new File("./autentication");
+			String[] files = currentDir.list();
+			URL[] xmls = new URL[files.length];
+			for (int i = 0; i < files.length; i++) {
+				try {
+					xmls[i] = (new File("./autentication/" + files[i])).toURI().toURL();
+				} catch (MalformedURLException ex) {
+					Logger.getLogger(AutenticationController.class.getName()).log(Level.SEVERE, null, ex);
+				}
 			}
-			
-			List<AutenticationBackEndCreator> autenticationCreators = core.getPluginController().getLoadedPluginsByType(AutenticationBackEndCreator.class);
-			
-			verifyClass(classe, autenticationCreators);
+			String classeName = null;
+			for (int i = 0; i < xmls.length; i++) {
+				DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = builderFactory.newDocumentBuilder();
+				Document documentAutentication = builder.parse(xmls[i].toString());
+				classeName = documentAutentication.getDocumentElement().getTextContent().trim();
+			}
+
+			List<AutenticationBackEndCreator> autenticationCreators = core.getPluginController()
+					.getLoadedPluginsByType(AutenticationBackEndCreator.class);
+			verifyClass(classeName, autenticationCreators);
 			this.autenticationBackEndCreator.anOperation();
-			
-			readFile.close();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -39,12 +57,12 @@ public class AutenticationController implements IAutenticationController{
 	public void setAutenticationBackCreator(AutenticationBackEndCreator autenticationBackEndCreator) {
 		this.autenticationBackEndCreator = autenticationBackEndCreator;
 	}
-	
+
 	private void verifyClass(String classe, List<AutenticationBackEndCreator> autenticationCreators) {
-		for(AutenticationBackEndCreator creator : autenticationCreators) {
-			String namePackageClass= creator.getClass().getName();
-			String nameClass= namePackageClass.substring(namePackageClass.lastIndexOf(".") + 1);
-			if(nameClass.equals(classe)) {
+		for (AutenticationBackEndCreator creator : autenticationCreators) {
+			String namePackageClass = creator.getClass().getName();
+			String nameClass = namePackageClass.substring(namePackageClass.lastIndexOf(".") + 1);
+			if (nameClass.equals(classe)) {
 				this.setAutenticationBackCreator(creator);
 			}
 		}
