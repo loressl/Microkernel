8127390c66354d30aa5c6b0e1abd28d81957d235
loressl <loryssl@hotmail.com>
2019-07-15 14:36:52 -0300

    Atualizado

--- Application/src/application/AutenticationController.java
+++ Application/src/application/AutenticationController.java
@@ -1,18 +1,14 @@
 package application;
 
 import java.io.File;
-import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
 
 import interfaces.AutenticationBackEndCreator;
 import interfaces.IAutenticationController;
@@ -22,7 +18,6 @@ public class AutenticationController implements IAutenticationController {
 
 	@Override
 	public boolean initialize(ICore core) {
-
 		try {
 			File currentDir = new File("./autentication");
 			String[] files = currentDir.list();
@@ -35,18 +30,17 @@ public class AutenticationController implements IAutenticationController {
 				}
 			}
 			String classeName = null;
+			List<AutenticationBackEndCreator> autenticationCreators = core.getPluginController()
+					.getLoadedPluginsByType(AutenticationBackEndCreator.class);
 			for (int i = 0; i < xmls.length; i++) {
 				DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder builder = builderFactory.newDocumentBuilder();
 				Document documentAutentication = builder.parse(xmls[i].toString());
 				classeName = documentAutentication.getDocumentElement().getTextContent().trim();
+				if (verifyClass(classeName, autenticationCreators))
+					break;
 			}
-
-			List<AutenticationBackEndCreator> autenticationCreators = core.getPluginController()
-					.getLoadedPluginsByType(AutenticationBackEndCreator.class);
-			verifyClass(classeName, autenticationCreators);
 			this.autenticationBackEndCreator.anOperation();
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -58,14 +52,16 @@ public class AutenticationController implements IAutenticationController {
 		this.autenticationBackEndCreator = autenticationBackEndCreator;
 	}
 
-	private void verifyClass(String classe, List<AutenticationBackEndCreator> autenticationCreators) {
+	private boolean verifyClass(String classe, List<AutenticationBackEndCreator> autenticationCreators) {
 		for (AutenticationBackEndCreator creator : autenticationCreators) {
 			String namePackageClass = creator.getClass().getName();
 			String nameClass = namePackageClass.substring(namePackageClass.lastIndexOf(".") + 1);
 			if (nameClass.equals(classe)) {
 				this.setAutenticationBackCreator(creator);
+				return true;
 			}
 		}
+		return false;
 	}
 
 	private AutenticationBackEndCreator autenticationBackEndCreator;
